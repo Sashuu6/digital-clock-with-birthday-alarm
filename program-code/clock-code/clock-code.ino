2376c546a4ea540bc1049ef94d0f6421435cba68
Sashwat K <sashwat0001@gmail.com>
2019-01-31 22:12:12 +0530

    made changes to OLED

--- program-code/clock-code/clock-code.ino
+++ program-code/clock-code/clock-code.ino
@@ -10,7 +10,7 @@
 // Author: Sashwat K
 // Created On: 17 Jan 2019
 // Revision: 1
-// Last Edited: 17 Jan 2019
+// Last Edited: 31 Jan 2019
 
 #include <SPI.h>  // adafruit display driver header file
 #include <Wire.h> // adafruit display driver header file
@@ -29,12 +29,16 @@ String current_time;
 String current_temperature;
 String your_birthday = "06.02";
 String temp_date = "00.00";
+int disp_screen = 0;
+int button_pin = 8;
+boolean button_value = 0;
 
 void setup() {
-  Serial.begin(9600); // Initialize Serial Monitor
+  pinMode(button_pin, INPUT);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // OLED
   display.display(); //initialize display
   display.clearDisplay(); //clear display
+  Serial.begin(9600); // Initialize Serial Monitor
 }
 
 void loop() {
@@ -54,19 +58,56 @@ void loop() {
       count++;
     }
   }
+
+  button_value = digitalRead(button_pin);
+  if(button_value == 1) {
+    disp_screen++;
+    if(disp_screen>2) {
+      disp_screen = 0;
+    }
+  }
+  
   display.clearDisplay();
   display.display();
-  display.setTextSize(1); // setting text size
   display.setTextColor(WHITE); // setting text color
-  display.setCursor(0,0); // setting position for OLED display
-  display.print(current_time);
-  display.print(" ");
-  display.println(current_day);
-  display.setCursor(0,10);
-  display.print(current_date);
-  display.print(" ");
-  display.print(current_temperature);
-  display.print(" C");
+  if(disp_screen == 0) {
+    display.setTextSize(2); // setting text size
+    display.setCursor(17,0); // setting position for OLED display
+    display.print(current_time);
+    display.setTextSize(1.5);
+    display.setCursor(10,20);
+    display.println(current_day);
+    display.setCursor(95,20);
+    if(count == 5) {
+      display.println(">>1<<");
+    }
+    else {
+      display.println(">>0<<");
+    }
+  }
+  else if(disp_screen == 1) {
+    display.setTextSize(2); // setting text size
+    display.setCursor(0,0); // setting position for OLED display
+    display.print(current_date);
+    display.setTextSize(1.5);
+    display.setCursor(40,20);
+    display.print(current_temperature);
+    display.print(" C");
+  }
+  else {
+    if(count == 5) {
+      display.setTextSize(2); // setting text size
+      display.setCursor(0,0); // setting position for OLED display
+      display.println("Happy Bday");
+      display.println("Vijitha !!");
+      display.display();
+    }
+    else {
+      display.setTextSize(2); // setting text size
+      display.setCursor(10,10); // setting position for OLED display
+      display.println("No alerts");
+    }
+  }
 
   //Printing all the stuff on the serial monitor
   // Shows the day
@@ -86,13 +127,16 @@ void loop() {
   
   //Show Temperature
   Serial.print("Temperature: ");
-  Serial.println(current_temperature);
+  Serial.print(current_temperature);
+  Serial.print(" ");
 
-  display.setCursor(0,20);
-  if(count == 5) { // Set your birthday date here
-    display.print("Happy birthday");
+  //Show Screen number
+  Serial.print("Screen number: ");
+  Serial.println(disp_screen);
+  //Show birthday or not
+  if(count == 5) {
     Serial.print(" ");
-    Serial.println("Happy birthday");
+    Serial.println("Happy Bday Vijitha");
   }
   display.display(); 
   
